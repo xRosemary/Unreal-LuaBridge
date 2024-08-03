c759efe71f8e33a1ecb24f2a56d369323a171ce1
xRosemary <2862656629@qq.com>
2024-08-03 22:17:08 +0800

    更新测试用例

--- MyActor.lua
+++ MyActor.lua
@@ -18,25 +18,30 @@ function MyActor:TestFunc()
 end
 
 function MyActor:TestFuncWithParam2(Param1, Param2, Param3)
+    if Param3 == false then
+        UE.error(Param3)
+        return 654
+    end
+
     self.TestVar = 987
     UE.error(self.TestVar)
     UE.error(self.LuaVar)
     UE.error(Param1)
     UE.error(Param2)
     UE.error(Param3)
-    -- UE.error(self.TestVar)
-    -- UE.error(self.MetaVal)
-    -- self:TestFunc()
-    -- UE.dump()
+    UE.error(self:GetTestVar2())
+    UE.error(self:GetTestVar3())
+    self:TestFuncWithParam2(Param1, Param2, false)
+    UE.dump()
     return 123
 end
 
--- function MyActor:GetTestVar2()
---     return 11
--- end
+function MyActor:GetTestVar2()
+    return 11
+end
 
--- function MyActor:GetTestVar3()
---     return 12
--- end
+function MyActor:GetTestVar3()
+    return 12
+end
 
 return MyActor
